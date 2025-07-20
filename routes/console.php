1b7b5c451986a31801ea9f850e0dee69de3cdada
Tebin Ulrich <dlhairbus320@googlemail.com>
2025-07-20 20:24:53 +0200

    Adds logrotation command

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('logs:rotate', function () {
+    $this->info('Rotating logs...');
+    exec('/usr/sbin/logrotate /etc/logrotate.d/laravel-log-rotate --state /var/lib/logrotate/status');
+    $this->info('Logs have been rotated.');
+})->purpose('Rotate Laravel logs using logrotate');
